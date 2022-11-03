5f80bf34e68f80cf89919c816d23dca3c43b3acc
Oscar Elenius <oscarelenius2002@gmail.com>
2022-11-03 14:06:06 +0100

    Task 4 done

---
+++ oscele_1_Lab1_Task4/Intermediate/Build/Win64/UE4Editor/Inc/oscele_1_Lab1_Task4/oscele_1_Lab1_Task4GameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef OSCELE_1_LAB1_TASK4_oscele_1_Lab1_Task4GameModeBase_generated_h
+#error "oscele_1_Lab1_Task4GameModeBase.generated.h already included, missing '#pragma once' in oscele_1_Lab1_Task4GameModeBase.h"
+#endif
+#define OSCELE_1_LAB1_TASK4_oscele_1_Lab1_Task4GameModeBase_generated_h
+
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_SPARSE_DATA
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_RPC_WRAPPERS
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAoscele_1_Lab1_Task4GameModeBase(); \
+	friend struct Z_Construct_UClass_Aoscele_1_Lab1_Task4GameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(Aoscele_1_Lab1_Task4GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/oscele_1_Lab1_Task4"), NO_API) \
+	DECLARE_SERIALIZER(Aoscele_1_Lab1_Task4GameModeBase)
+
+
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAoscele_1_Lab1_Task4GameModeBase(); \
+	friend struct Z_Construct_UClass_Aoscele_1_Lab1_Task4GameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(Aoscele_1_Lab1_Task4GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/oscele_1_Lab1_Task4"), NO_API) \
+	DECLARE_SERIALIZER(Aoscele_1_Lab1_Task4GameModeBase)
+
+
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API Aoscele_1_Lab1_Task4GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(Aoscele_1_Lab1_Task4GameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, Aoscele_1_Lab1_Task4GameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(Aoscele_1_Lab1_Task4GameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API Aoscele_1_Lab1_Task4GameModeBase(Aoscele_1_Lab1_Task4GameModeBase&&); \
+	NO_API Aoscele_1_Lab1_Task4GameModeBase(const Aoscele_1_Lab1_Task4GameModeBase&); \
+public:
+
+
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API Aoscele_1_Lab1_Task4GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API Aoscele_1_Lab1_Task4GameModeBase(Aoscele_1_Lab1_Task4GameModeBase&&); \
+	NO_API Aoscele_1_Lab1_Task4GameModeBase(const Aoscele_1_Lab1_Task4GameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, Aoscele_1_Lab1_Task4GameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(Aoscele_1_Lab1_Task4GameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(Aoscele_1_Lab1_Task4GameModeBase)
+
+
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_12_PROLOG
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_SPARSE_DATA \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_RPC_WRAPPERS \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_INCLASS \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_SPARSE_DATA \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> OSCELE_1_LAB1_TASK4_API UClass* StaticClass<class Aoscele_1_Lab1_Task4GameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID oscele_1_Lab1_Task4_Source_oscele_1_Lab1_Task4_oscele_1_Lab1_Task4GameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ oscele_1_Lab1_Task4/Source/oscele_1_Lab1_Task4/oscele_1_Lab1_Task4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "oscele_1_Lab1_Task4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, oscele_1_Lab1_Task4, "oscele_1_Lab1_Task4" );
